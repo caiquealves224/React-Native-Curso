3ef14bbdc3112d45c6c951b67b7c2e0339b0fa87
caiquealves224 <caiquealves224@gmail.com>
2019-07-31 20:41:42 -0300

    aplicando estili e importando componente para o menu

--- exercicios/src/Menu.js
+++ exercicios/src/Menu.js
@@ -6,8 +6,13 @@ import ParImpar from './components/ParImpar';
 import Contador from './components/Contador';
 import Plataforma from './components/Plataforma';
 import ValidarProps from './components/ValidarProps'
+import Evento from './components/Evento'
 
 export default createDrawerNavigator({
+    Evento : {
+        screen : () => <Evento/>
+    },
+    
     ValidarProps : {
         screen : () => <ValidarProps  ano={18}/>
     },
--- exercicios/src/components/Evento.js
+++ exercicios/src/components/Evento.js
@@ -3,5 +3,26 @@ import { View, Text, TextInput } from 'react-native';
 import Padrao from '../Estilo/Padrao';
 
 export default class Evento extends Component {
-    
+    state = {
+        texto: ''
+    }
+
+    alterarTexto = texto => {
+        this.setState({ texto })
+    }
+
+    render(){
+        return(
+            <View>
+                <Text style={Padrao.fonte40}>{this.state.texto}</Text>
+                <TextInput 
+                    value={this.state.texto}
+                    style={Padrao.input}
+                    onChangeText={this.alterarTexto}
+                />
+                    
+            </View>
+        )
+
+    }
 }

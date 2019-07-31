a8ec4c60c7b44c5b57afc7af5f2725cdf20846d5
Caique Alves <caiquealves224@gmail.com>
2019-07-31 20:05:40 -0300

    criar componente Evento

---
+++ exercicios/src/components/Evento.js
@@ -0,0 +1,7 @@
+import React , { Component } from 'react';
+import { View, Text, TextInput } from 'react-native';
+import Padrao from '../Estilo/Padrao';
+
+export default class Evento extends Component {
+    
+}
